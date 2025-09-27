048dc88b269437c3171eff8798ee73fac72a2bb2
Óscar Vera López <oscar.veral@um.es>
2025-09-27 19:25:07 +0200

    Changed stow's executing user.

--- leopard/config/user.sh
+++ leopard/config/user.sh
@@ -18,18 +18,25 @@ unset PASSPHRASE
 
 # Create user directories.
 
-mkdir -p /home/oscar
+USER_HOME="/home/oscar"
+if [ ! -d "$USER_HOME" ]; then
+    printf "Creating home directory for user oscar...\n"
+    mkdir -p "$USER_HOME" || abort "create home directory for user oscar"
+    chown oscar:oscar "$USER_HOME" || abort "set owner of home directory for user oscar"
+    chmod 754 "$USER_HOME" || abort "set permissions of home directory for user oscar"
+fi
 
 # Copy this dotfiles repository to the user's home.
 
-if [ -d /home/oscar/dotfiles ]; then
-    printf "Directory /home/oscar/dotfiles already exists. Removing it first.\n"
-    rm -rf /home/oscar/dotfiles || abort "remove existing /home/oscar/dotfiles"
+if [ -d "$USER_HOME/dotfiles" ]; then
+    printf "Directory $USER_HOME/dotfiles already exists. Removing it first.\n"
+    rm -rf "$USER_HOME/dotfiles" || abort "remove existing $USER_HOME/dotfiles"
 fi
-cp -r "$DOTFILES_LOCAL_PATH" /home/oscar/dotfiles || abort "copy dotfiles to user oscar home"
-chown -R oscar:oscar /home/oscar/dotfiles || abort "change owner of dotfiles to user oscar"
-chmod -R 744 /home/oscar/dotfiles || abort "set perms of dotfiles to 744"
+cp -r "$DOTFILES_LOCAL_PATH" "$USER_HOME/dotfiles" || abort "copy dotfiles to user oscar home"
+chown -R oscar:oscar "$USER_HOME/dotfiles" || abort "change owner of dotfiles to user oscar"
+chmod -R 744 "$USER_HOME/dotfiles" || abort "set perms of dotfiles to 744"
 
 # Set up the user's home directory with stow.
 
-stow -d /home/oscar/dotfiles/leopard/files/home -t /home/oscar -R oscar || abort "stow user oscar home files"
+su oscar -c "stow -d $USER_HOME/dotfiles/leopard/files/home -t $USER_HOME -R oscar" || abort "stow user oscar home files"
+rm -rf "$USER_HOME"/.bash_history
