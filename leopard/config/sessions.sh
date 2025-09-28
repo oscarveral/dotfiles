ffe8dac19be66de440b3856545fc0a1d58d58639
Óscar Vera López <oscar.veral@um.es>
2025-09-28 21:10:16 +0200

    Forgot to copy the new session.

--- leopard/config/sessions.sh
+++ leopard/config/sessions.sh
@@ -11,3 +11,4 @@ cp "$MACHINE_PATH/files/etc/ly/config.ini" /etc/ly/config.ini || abort "copy ly 
 # Copy wayland session file.
 rm -rf /usr/share/wayland-sessions/*
 cp "$MACHINE_PATH/files/usr/share/wayland-sessions/hyprland.desktop" /usr/share/wayland-sessions/hyprland.desktop || abort "copy hyprland session"
+cp "$MACHINE_PATH/files/usr/share/wayland-sessions/hyprland-uwsm.desktop" /usr/share/wayland-sessions/hyprland-uwsm.desktop || abort "copy hyprland-uwsm session"
